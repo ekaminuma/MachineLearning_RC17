0be32ce771160e2846cb0f7f81484529116373ad
U-DESKTOP-19GMTNK\ekaminuma <ekaminum@nig.ac.jp>
2018-03-16 16:58:11 +0900

    180226

--- saitobook/common/layers.py
+++ saitobook/common/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from common.functions import *
-from common.util import im2col, col2im
+from saitobook.common.functions import *
+from saitobook.common.util import im2col, col2im
 
 
 class Relu:
